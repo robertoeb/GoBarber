98cb113f2cb1b4296ba67b05ca0e0b5bde116901
Roberto Eustaquio <robertoeb.jr@gmail.com>
2020-04-07 22:18:35 -0300

    Add appointments and schedule routes

---
+++ backend/src/app/controllers/ScheduleController.js
@@ -0,0 +1,48 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+import File from '../models/File';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'User is not a provider.' });
+    }
+
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      attributes: ['id', 'date', 'canceled_at'],
+      include: [
+        { model: User, as: 'user', attributes: ['id', 'name'] },
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            { model: File, as: 'avatar', attributes: ['id', 'path', 'url'] },
+          ],
+        },
+      ],
+      order: ['date'],
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
