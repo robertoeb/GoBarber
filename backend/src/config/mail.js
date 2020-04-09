c1d0a32d156d96ff57106f252462ac8afde84d0d
Roberto Eustaquio <robertoeb.jr@gmail.com>
2020-04-08 22:20:31 -0300

    Add cancellation mail and jobs queue

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '65569eae19bdd7',
+    pass: 'a93d758d674727',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
